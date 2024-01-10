c026f342e970703d58dd3ba4ca89a5689bf3771d
inglorious-ratbastard <47618267+inglorious-ratbastard@users.noreply.github.com>
2024-01-10 20:42:53 +0000

    update fix 2024_1

--- app.js
+++ app.js
@@ -1,29 +1,21 @@
 var express = require('express');
-
 var app = express();
-
+const port = process.env.PORT || 8000;
 
 app.set('view engine', 'ejs');
-
 
 var routes = require('./routes');
 
 var path = require('path');
 app.use(express.static(path.join(__dirname, 'public')));
 
-
-
-//Routes
-
-//home
 app.get('/', routes.home);
 
-//movie single
 app.get('/star_wars_episode/:episode_number?', routes.movie_single);
 
-//notFound
 app.get("*", routes.notFound);
 
-
-app.listen(process.env.PORT || 3000);
-  
+app.listen(() => {
+  console.log(`Listening at http://localhost:${port}`);
+  console.log("R2-D2, you know better than to trust a strange computer");
+})
